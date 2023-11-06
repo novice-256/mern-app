d2be25719acfd920e80c9ff4861c876e33a8018f
novice-256 <145491297+novice-256@users.noreply.github.com>
2023-11-06 13:58:02 -0800

    Update index.js
    modified code that should work with __dirname in a CommonJS module

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import routes from "./Routes/router.js";
-import connection from "./Database/connection.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import path from "path";
-import { env } from "process";
+const express = require('express');
+const path = require('path');
+const routes = require('./Routes/router.js');
+const connection = require('./Database/connection.js');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 // Allow Cross-Origin Resource Sharing (CORS)
@@ -14,19 +14,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Serve static files from the "public/Images" directory
-app.use(express.static(path.resolve('public')));
+app.use(express.static(path.resolve('public'));
 
 connection();
 
 // Define your routes
 app.use('/api', routes);
-// We need to tell the Heroku to serve the statics files of the client.
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("sample_app/build"))
-    app.get("*",(req,res)=>{
-        // res.sendFile(path.resolve('sample_app/build/index.html')
-        res.sendFile(path.resolve(__dirname,'sample_app','build','index.html'))
-    })
+
+// We need to tell Heroku to serve the static files of the client.
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('sample_app/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'sample_app', 'build', 'index.html'));
+  });
 }
- const PORT = process.env.PORT || 8000
-app.listen(PORT, () => console.log("Server running at PORT: " + PORT));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log('Server running at PORT: ' + PORT));
