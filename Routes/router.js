963aa450ecf0721d5d21a206b3057a9a665f3e10
novice-256 <145491297+novice-256@users.noreply.github.com>
2023-11-06 07:42:57 -0800

    Update router.js

--- Routes/router.js
+++ Routes/router.js
@@ -9,7 +9,7 @@ import { existsSync } from 'node:fs';
 const router= express.Router();
 // router.post('/signup',add)
 router.get('/signup',createToken)
-router.post('signup/file=:size%:type%:ext', imageValidation,uploadFile.single("uplFile"),validateToken,add)
+router.post('/signup/file=:size%:type%:ext', imageValidation,uploadFile.single("uplFile"),validateToken,add)
 router.post('/auth=:role/delete',authMidlWare,remove)
 router.get('/auth=:role/view/:page',authMidlWare,fetch)
 router.post('/auth=:role/update/:id/:file',uploadFile.single("uplFile"),authMidlWare,update)
