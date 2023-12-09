be11b0092ab2876d24ac2853f8aaac1dba3e0789
novice-256 <145491297+novice-256@users.noreply.github.com>
2023-12-09 22:13:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.resolve('public')));
 connection();
 
 // Define your routes
-app.use('/api', routes);
+app.use('/', routes);
 // We need to tell the Heroku to serve the statics files of the client.
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("sample_app/build"))
