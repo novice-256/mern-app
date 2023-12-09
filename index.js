6b6d81d7e96c0d894b96019811abd3bc721c23de
novice-256 <145491297+novice-256@users.noreply.github.com>
2023-12-09 22:02:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { env } from "process";
 const app = express();
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
+
 // Allow Cross-Origin Resource Sharing (CORS)
-// app.use(cors({ credentials: true, origin: 'https://emp-app-ca789b749d5f.herokuapp.com' }));
+app.use(cors({ credentials: true, origin: 'https://emp-app-ca789b749d5f.herokuapp.com' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,16 +21,13 @@ connection();
 // Define your routes
 app.use('/api', routes);
 // We need to tell the Heroku to serve the statics files of the client.
-
-
-
-          
-    app.use(express.static("./sample_app/build"))
- app.get("*", (req, res) => {
-  const path_to_index= path.join('sample_app','build','index.html');
-console.log(`./${path_to_index}`);
-  res.sendFile(`./${path_to_index}`);
-});
-
- const PORT = process.env.PORT || 3000
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("sample_app/build"))
+    app.get("*",(req,res)=>{
+      const __dirname = path.resolve('sample_app');
+         res.sendFile('index.html', { root: __dirname });
+// res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    })
+}
+ const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log("Server running at PORT: " + PORT));
